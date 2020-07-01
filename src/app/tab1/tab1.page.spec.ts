fe7f6fb3c48404d180bcd86b86d623cee343fe72
TXT-50 <txt-50@MacBook-Pro-4.local>
2020-06-30 23:05:20 -0700

    I deleted the pictures for the app we are planning on keeping the app simple and just having text in order to show the reader what the source will be about

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,3 +1,4 @@
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
