886cf251da9bba73fc31305238088dd4765a766b
TXT-50 <txt-50@MacBook-Pro-4.local>
2020-07-02 23:14:00 -0700

    I uploaded the images for the resources. All the resources open on a different page but with the emulator it opens up in the app.

--- src/app/resources/resources.page.ts
+++ src/app/resources/resources.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ InAppBrowser } from '@ionic-native/in-app-browser/ngx'
 
 @Component({
   selector: 'app-resources',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourcesPage implements OnInit {
 
-  constructor() { }
+  // constructor(private lab: InAppBrowser) {
+
+  constructor(private lab: InAppBrowser,) { }
 
   ngOnInit() {
   }
+
+openBlank() {
+  this.lab.create(`https://www.pcworld.com/article/2957260/how-to-install-an-intel-or-amd-cpu-in-your-computer.html#:~:text=Start%20with%20your%20motherboard%20outside,LGA%20socket%20in%20the%20motherboard.&text=Now`, `_blank`);
+}
+
+openBlank2() {
+  this.lab.create(`https://youtu.be/POv93iBvUkI`, `_blank`);
+}
+
+openBlank3() {
+  this.lab.create(`https://www.reddit.com/r/buildapc/comments/6a9daa/need_help_with_connecting_cpu_fan_to_motherboard/`, `_blank`);
+}
+
+
 
 }
